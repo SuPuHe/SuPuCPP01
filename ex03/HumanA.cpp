12308772fb6abb0d63bbcf496585febd085d3f8d
SuPuHe <mizin.a.s.2006@gmail.com>
2025-09-05 17:24:46 +0200

    create HumanA.hpp and HumanA.cpp

---
+++ ex03/HumanA.cpp
@@ -0,0 +1,11 @@
+#include "HumanA.hpp"
+#include "Weapon.hpp"
+
+HumanA::HumanA(std::string n, Weapon &w) : name(n), weapon(w) {}
+
+HumanA::~HumanA(){}
+
+void HumanA::attack(void)
+{
+	std::cout << name << " attacks with their " << weapon.getType() << std::endl;
+}
---
+++ ex03/HumanA.hpp
@@ -0,0 +1,18 @@
+#ifndef HUMANA_HPP
+#define HUMANA_HPP
+
+#include "Weapon.hpp"
+
+class HumanA
+{
+	private:
+		std::string name;
+		Weapon &weapon;
+	public:
+		HumanA(std::string n, Weapon &w);
+		~HumanA();
+
+		void attack(void);
+};
+
+#endif
