c4b93dd0254bf97532908a295bcc3a55356a085f
SuPuHe <mizin.a.s.2006@gmail.com>
2025-09-05 15:07:38 +0200

    created Zombie.cpp

---
+++ ex00/Zombie.cpp
@@ -0,0 +1,18 @@
+#include "Zombie.hpp"
+
+Zombie::Zombie(std::string name) : this->name(name)
+{
+	std::cout << "A zombie with name " << this->name
+			<< " was created" << std::endl;
+}
+
+Zombie::~Zombie()
+{
+	std::cout << "A zombie with name " << name
+			<< " was deleted" << std::endl;
+}
+
+void Zombie::announce(void)
+{
+	std::cout << name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+}
