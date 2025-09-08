ef71036913117e13acffa2202ef356f3afc46d0f
SuPuHe <mizin.a.s.2006@gmail.com>
2025-09-08 13:43:53 +0200

    finish ex04

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,11 +6,12 @@
 /*   By: omizin <omizin@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/08 11:33:04 by omizin            #+#    #+#             */
-/*   Updated: 2025/09/08 11:42:18 by omizin           ###   ########.fr       */
+/*   Updated: 2025/09/08 13:12:17 by omizin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <fstream>
 
 int	main(int argc, char **argv)
 {
@@ -19,8 +20,50 @@ int	main(int argc, char **argv)
 		std::string filename = argv[1];
 		std::string s1 = argv[2];
 		std::string s2 = argv[3];
+
+		if (s1.empty())
+		{
+			std::cerr << "Line can't be empty" << std::endl;
+			return (1);
+		}
+
+		std::ifstream file(filename);
+		if (!file)
+		{
+			std::cerr << "Error opening file" << std::endl;
+			return (1);
+		}
+		std::string line;
+		std::string replace_filename = filename + ".replace";
+		std::ofstream replace_file(replace_filename);
+		if (!replace_file)
+		{
+			std::cerr << "Error opening replace file" << std::endl;
+			return (1);
+		}
+		while (std::getline(file, line))
+		{
+			std::string result;
+			for (size_t i = 0; i < line.size();)
+			{
+				if (line.compare(i, s1.size(), s1) == 0)
+				{
+					result += s2;
+					i += s1.size();
+				}
+				else
+				{
+					result += line[i];
+					i++;
+				}
+			}
+			replace_file << result << std::endl;
+		}
+		file.close();
+		replace_file.close();
 	}
 	else
-		std::cout << "usage: ./sed filename s1 s2" << std:: endl;
+		std::cerr << "usage: ./sed filename s1 s2" << std:: endl;
+
 	return (0);
 }
